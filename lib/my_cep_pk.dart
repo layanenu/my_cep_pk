497a05ab250662921b20870d1502d75d28a84b11
layanenu <layanenu@gmail.com>
2024-06-19 22:47:57 -0300

    first commit

---
+++ lib/my_cep_pk.dart
@@ -0,0 +1,33 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class CepService {
+  static Future<String?> carregarResultadoSalvo() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('endereco_salvo');
+  }
+
+  static Future<void> salvarResultado(String resultado) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('endereco_salvo', resultado);
+  }
+
+  static Future<String> pesquisarCep(String cep) async {
+    var url = Uri.parse('https://viacep.com.br/ws/$cep/json/');
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var data = json.decode(response.body);
+      if (data['erro'] == null) {
+        String endereco =
+            'Endereço: ${data['logradouro']}, ${data['bairro']}, ${data['localidade']}, ${data['uf']}';
+        await salvarResultado(endereco);
+        return endereco;
+      } else {
+        return 'CEP não encontrado.';
+      }
+    } else {
+      return 'Erro na consulta. Tente novamente.';
+    }
+  }
+}
